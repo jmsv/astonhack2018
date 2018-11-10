9b985cc6f463c4161466178459af6327ddce117b
James Vickery <dev@jamesvickery.net>
2018-11-10 16:49:04 +0000

    server spotify key env

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const port = 3001
 const cors = require('cors')
 
+const spotify = require('./spotify')
+
 app.use(cors())
 
-app.get('/', (req, res) => res.send('ok'))
+
+app.get('/', (_, res) => res.send('ok'))
 
 app.listen(port, () => console.log(`spartify running on ${port}`))
---
+++ server/spotify.js
@@ -0,0 +1,6 @@
+const client = {
+  id: process.env.CLIENT_ID,
+  secret: process.env.CLIENT_SECRET
+}
+
+console.log(client)

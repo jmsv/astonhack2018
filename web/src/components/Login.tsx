187acbd9b1e2e97575c7ed8cee2d93460f4349f0
James Vickery <dev@jamesvickery.net>
2018-11-11 13:12:39 +0000

    login link

--- web/src/components/Login.tsx
+++ web/src/components/Login.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 
+import { Link } from 'react-router-dom'
 
 class Login extends React.Component {
   public render() {
     return (
       <div>
-        login
+        <Link to='https://spartify.xyz/api/auth/spotify'>Login with Spotify</Link>
       </div>
     );
   }
